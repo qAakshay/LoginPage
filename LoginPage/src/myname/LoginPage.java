695383c2ed5e82826e312caf5068cf1910723b99
Akshay Kokare <akshaykokare15062001@gmail.com>
2023-03-28 12:04:28 +0530

    this is LoginPage
    this good is right go ahead

---
+++ LoginPage/src/myname/LoginPage.java
@@ -0,0 +1,11 @@
+package myname;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("this is loginpage");
+        System.out.println("akshaykokare35@gmail.com");
+	}
+
+}
